495d209f711e7721c5ed20c2299e40ddb5351020
Frank Oder <frank.oder@live.de>
2021-08-25 09:48:04 +0200

    update

--- main.tf
+++ main.tf
@@ -1,12 +1,33 @@
-provider"azurerm"{
-    version = "2.2.0"
-    features {}
+provider "azurerm" {
+  # AzureRM provider 2.x
+  version = "~>2.0"
+  # v2.x required "features" block
+  features {}
 }
 
-resource "azurerm_resource_group" "web_server_rg" {
-    name = "web-rg"
-    location = "westus2"
+# Create a resource group
+resource "azurerm_resource_group" "MMS_IOT_FO" {
+  name     = "mms-iot-rg"
+  location = "North Central US"
+  tags = {
+    test = "Frank_MMS"
+  }
 }
+
+
+# Create an Azure IoT Hub
+resource "azurerm_iothub" "mmsiothubfo" {
+  name                = "mmsiothubfo"
+  resource_group_name = azurerm_resource_group.MMS_IOT_FO.name
+  location            = "North Central US"
+
+  sku {
+    name     = "S1"
+    capacity = 1
+  }
+}
+
+
 
 
 
