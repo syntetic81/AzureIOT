2a1db306dfe86a33fd67f9b1d2d2a65d1f49c810
Frank Oder <frank.oder@live.de>
2021-08-25 11:23:38 +0200

    update

--- variables.tf
+++ variables.tf
@@ -1,15 +1,21 @@
 variable "subscription id" {
-  
+    description = "Subscription ID"
+    type = string
+    default = "a7847ccc-b9c4-4d95-8936-040dddfd84e6"  
 }
 
 variable "client_id" {
-  
+    description = "Client ID"
+    type = string  
 }
 
 variable "tenant_id" {
-  
+    description = "Tenant ID"
+    type = string
+    default = "084da262-1f10-4d11-88a8-7ef210eafc02"    
 }
 
 variable "client_secret" {
-  
+    description = "Client Secret"
+    type = string  
 }
