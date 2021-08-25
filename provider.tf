b46b714cb4328425f85c17e73d7b032466c3fd76
Frank Oder <frank.oder@live.de>
2021-08-25 10:52:09 +0200

    update

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+      required_providers {
+          azurem= {
+    source = "hashicorp/azurem"
+    version = "=2.46.0"
+    }
+    }
+}
+
+provider "azurerm" {
+    
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "subscription id" {
+  
+}
+
+variable "client_id" {
+  
+}
+
+variable "tenant_id" {
+  
+}
+
+variable "client_secret" {
+  
+}
